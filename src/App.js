d9da5aac2ee63eeac9f7223f6f8948d95a26b5da
liulei <liulei1@tianyancha.com>
2021-07-09 15:37:56 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect,useContext} from 'react'
+// function Counter({initialCount}) {
+//     console.log(initialCount)
+//     const [count, setCount] = useState(initialCount);
+//     return (
+//         <>
+//             Count: {count}
+//             <button onClick={() => setCount(initialCount)}>Reset</button>
+//             <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+//             <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+//         </>
+//     );
+// }
+// function App() {
+//
+//   return (
+//     <div className="App">
+//         {Counter({initialCount:4})}
+//     </div>
+//   );
+// }
+
+const themes = {
+    light: {
+        foreground: "#000000",
+        background: "#eeeeee"
+    },
+    dark: {
+        foreground: "#ffffff",
+        background: "#222222"
+    }
+};
+//react中的状态共享的几种方式
+
+
+const ThemeContext = React.createContext(themes.light);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.myRef=React.createRef()
+    return (
+        <ThemeContext.Provider value={themes.dark}>
+            <Toolbar ref={this.myRef} />
+        </ThemeContext.Provider>
+    );
 }
 
+function Toolbar(props) {
+
+    return (
+        <div>
+            <ThemedButton   />
+        </div>
+    );
+}
+
+function ThemedButton() {
+    console.log(ThemeContext)
+    const theme = useContext(ThemeContext);
+    return (
+        <button style={{ background: theme.background, color: theme.foreground }}>
+            I am styled by theme context!
+        </button>
+    );
+}
 export default App;
